879ad70c9a24bf57f3922ab34501a89becb0c4d4
Sebastián Villavicencio <111655076+Ryxxo@users.noreply.github.com>
2025-06-30 16:02:03 -0600

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,21 +1,9 @@
-# import pyrebase
 import firebase_admin
-from firebase_admin import credentials, auth
+from firebase_admin import credentials, auth, firestore
 
+# Inicializa Firebase con la clave de servicio
 cred = credentials.Certificate("firebase_key.json")
 firebase_admin.initialize_app(cred)
 
-
-firebase_config = {
-    "apiKey": "AIzaSyBorRTmuKdUp93J87FeuenqrGgAUNmXYyc",
-    "authDomain": "plents-9fcf2.firebaseapp.com",
-    "databaseURL": "https://plents-9fcf2-default-rtdb.firebaseio.com",
-    "projectId": "plents-9fcf2",
-    "storageBucket": "plents-9fcf2.appspot.com",
-    "messagingSenderId": "536770589606",
-    "appId": "1:536770589606:web:6d7cb555d8047cf3891567",
-    "measurementId": "G-0KCGX1QDHW"
-}
-
-firebase = pyrebase.initialize_app(firebase_config)
-auth = firebase.auth()
+# Firestore y Auth quedan disponibles así:
+db = firestore.client()

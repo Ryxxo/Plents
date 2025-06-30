ae7ef4088a35faec006ead175942b556bb5d59dd
Sebastián Villavicencio <111655076+Ryxxo@users.noreply.github.com>
2025-06-30 16:07:18 -0600

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,9 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
 
-# Inicializa Firebase con la clave de servicio
-cred = credentials.Certificate("firebase_key.json")
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    cred = credentials.Certificate("firebase_key.json")
+    firebase_admin.initialize_app(cred)
 
-# Firestore y Auth quedan disponibles así:
 db = firestore.client()

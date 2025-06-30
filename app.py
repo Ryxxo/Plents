2c7c25b5b3acc8e78a34927c878ddc8e5b9d00f1
Sebastián Villavicencio <111655076+Ryxxo@users.noreply.github.com>
2025-06-30 16:34:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -171,5 +171,10 @@ def profile():
     user = current_user
     return render_template("profile.html", user=user)
 
+@app.route("/")
+def index():
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
